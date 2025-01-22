82bda3d8074fd7202e436b206067cdb39a26a1fb
arjungravi1@gmail.com <arjungravi1@gmail.com>
2025-01-22 17:37:02 +0530

    docs: more docs

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+# This lets the repo to be installed using 'pip install -e .' at root
+setup(
+    name="LMBenchmarkingTool",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],  # put 
+)
