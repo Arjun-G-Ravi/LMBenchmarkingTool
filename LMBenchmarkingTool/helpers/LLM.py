eb4357fda0e6f9432e3865a69b83d5c0bb1fd151
arjungravi1@gmail.com <arjungravi1@gmail.com>
2025-01-24 06:03:37 +0530

    feat: added

--- LMBenchmarkingTool/helpers/LLM.py
+++ LMBenchmarkingTool/helpers/LLM.py
@@ -11,26 +11,42 @@ class LLM:
         self.temperature = temperature
         self.max_length = max_length
         self.base_prompt = base_prompt
+        self.return_loss = calculate_loss
 
-        # if calculate_loss:
-        #     self
+        if self.return_loss:
+            from transformers import AutoTokenizer, AutoModelForCausalLM
+            self.model = AutoModelForCausalLM.from_pretrained(model_name)
+            self.tokenizer =  AutoTokenizer.from_pretrained(model_name)
 
     def generate_response(self, text, add_base_prompt=True):
         if add_base_prompt: return self.generator(self.base_prompt+text, num_return_sequences=1)[0]['generated_text']
         else: return self.generator(text, num_return_sequences=1)[0]['generated_text']
     
     def calculate_loss(self, text, add_base_prompt=True):
-        if add_base_prompt: text = self.base_prompt+text
-        self.generator.loss
+        assert self.return_loss == True, 'Set the LLM parameter return_loss = True'
+        import torch
 
+        if add_base_prompt: text = self.base_prompt+text
+        inputs = self.tokenizer(text, return_tensors="pt", add_special_tokens=True)
+        
+        # Compute loss
+        with torch.no_grad():
+            outputs = self.model(
+                input_ids=inputs['input_ids'], 
+                attention_mask=inputs['attention_mask'], 
+                labels=inputs['input_ids']
+            )
+            loss = outputs.loss.item()
+        
+            return loss
 
 
 
 
 if __name__ == '__main__':
-    model = LLM("Arjun-G-Ravi/chat-GPT2")
+    model = LLM("Arjun-G-Ravi/chat-GPT2", calculate_loss=True)
+    print("Loaded model")
 
     # model = LLM("gpt2")
-    print(model.generate_response("""Your answers should not include any unethical, racist, sexist, dangerous, or illegal content. If the question is wrong, or does not make sense, accept it instead of giving the wrong answer.
-Question:What is your name?
-Answer: """))
+    print(model.calculate_loss("""Your answers should not include any unethical, racist, sexist, dangerous, or illegal content. If the question is wrong, or does not make sense, accept it instead of giving the wrong answer.
+Question: """))
