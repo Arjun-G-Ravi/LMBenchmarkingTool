c63340a475c43ef64296bee3e8f674076419e5aa
Arjun-G-Ravi <arjungravi1@gmail.com>
2025-11-28 10:14:23 +0530

    chore: a bit more reorder

--- LMBenchmarkingTool/__init__.py
+++ LMBenchmarkingTool/__init__.py
@@ -1,4 +1,4 @@
 from .src.LLM import LLM
 from .src.Benchmark import Benchmark
-from .src.helper_functions import prompting_template1
-from .src.helper_functions import _progress_bar
+from .utils.helper_functions import prompting_template1
+from .utils.helper_functions import _progress_bar
--- LMBenchmarkingTool/playground/demo.py
+++ LMBenchmarkingTool/playground/demo.py
@@ -1,7 +1,4 @@
 from LMBenchmarkingTool import Benchmark, LLM
 model = LLM('Qwen/Qwen2.5-3B-Instruct')
-# benchmark = Benchmark('mmlu')
 benchmark = Benchmark('mmlu')
-
-
 benchmark.run(model, num_samples=100) 
